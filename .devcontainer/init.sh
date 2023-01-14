b08faa3f69432788b71b9a1c6c7c34a8d64284d8
Gage Miller <gagemiller155@gmail.com>
2023-01-14 10:38:47 -0500

    Add devcontainer env

---
+++ .devcontainer/init.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+mkdir -p ~/docker/isaac-sim/cache/kit
+mkdir -p ~/docker/isaac-sim/cache/ov
+mkdir -p ~/docker/isaac-sim/cache/pip
+mkdir -p ~/docker/isaac-sim/cache/glcache
+mkdir -p ~/docker/isaac-sim/cache/computecache
+mkdir -p ~/docker/isaac-sim/logs
+mkdir -p ~/docker/isaac-sim/logs-kit
+mkdir -p ~/docker/isaac-sim/config
+mkdir -p ~/docker/isaac-sim/data
+mkdir -p ~/docker/isaac-sim/documents
+xhost +
---
+++ .devcontainer/postCreate.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+USERNAME=vscode
+
+# Take ownership of persistent data folders
+sudo chown -R $USERNAME:$USERNAME /home/$USERNAME/.cache
+sudo chown -R $USERNAME:$USERNAME /home/$USERNAME/.local
+sudo chown -R $USERNAME:$USERNAME /home/$USERNAME/.nv
+sudo chown -R $USERNAME:$USERNAME /home/$USERNAME/.nvidia-omniverse
+
+# Take ownership of isaac-sim folders
+sudo chown $USERNAME:$USERNAME /isaac-sim/kit
+sudo chown -R $USERNAME:$USERNAME /isaac-sim/kit/logs
+sudo chown $USERNAME:$USERNAME /isaac-sim/exts
+sudo chown -R $USERNAME:$USERNAME /isaac-sim/exts/omni.isaac.synthetic_recorder
+
+# Configure omniverse cache 
+cp ./.devcontainer/omniverse.toml /home/$USERNAME/.nvidia-omniverse/config/omniverse.toml
+
+# Run the postinstall script for isaac sim
+# Check to see if post install has already been run
+if [ ! -d ~/.cache/ov/Kit ]; then
+    echo "ISAAC SIM POST INSTALL RUNNING... THIS WILL TAKE ABOUT 10min"
+    bash /isaac-sim/omni.isaac.sim.post.install.sh
+fi                                                                                                                                                                                                                                                                                                      
+
+# Setup ZSH
+echo 'alias isaac=/isaac-sim/isaac-sim.sh' >> ~/.zshrc
+echo 'source /ros2_humble/install/setup.zsh' >> ~/.zshrc
+
+# Setup ROS2 Humble as the default bridge extension
+basekit_file="/isaac-sim/apps/omni.isaac.sim.base.kit"
+sed -i 's/ros_bridge/ros2_bridge-humble/' $basekit_file
