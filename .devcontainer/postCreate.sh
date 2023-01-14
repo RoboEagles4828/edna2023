f21e773135f228b44d4c2106a0d69dd0959861ab
ugackMiner <ugackMiner@gmail.com>
2023-01-14 19:22:54 +0000

    Isaac sim extensions now load - ROS2 is no longer souced in the `~/.zshrc` - The install script is now run with `sudo`

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -8,10 +8,11 @@ sudo chown -R $USERNAME:$USERNAME /home/$USERNAME/.nv
 sudo chown -R $USERNAME:$USERNAME /home/$USERNAME/.nvidia-omniverse
 
 # Take ownership of isaac-sim folders
-sudo chown $USERNAME:$USERNAME /isaac-sim/kit
-sudo chown -R $USERNAME:$USERNAME /isaac-sim/kit/logs
-sudo chown $USERNAME:$USERNAME /isaac-sim/exts
-sudo chown -R $USERNAME:$USERNAME /isaac-sim/exts/omni.isaac.synthetic_recorder
+# (Might want to uncomment later (untested))
+# sudo chown $USERNAME:$USERNAME /isaac-sim/kit
+# sudo chown -R $USERNAME:$USERNAME /isaac-sim/kit/logs
+# sudo chown $USERNAME:$USERNAME /isaac-sim/exts
+# sudo chown -R $USERNAME:$USERNAME /isaac-sim/exts/omni.isaac.synthetic_recorder
 
 # Configure omniverse cache 
 cp ./.devcontainer/omniverse.toml /home/$USERNAME/.nvidia-omniverse/config/omniverse.toml
@@ -20,12 +21,11 @@ cp ./.devcontainer/omniverse.toml /home/$USERNAME/.nvidia-omniverse/config/omniv
 # Check to see if post install has already been run
 if [ ! -d ~/.cache/ov/Kit ]; then
     echo "ISAAC SIM POST INSTALL RUNNING... THIS WILL TAKE ABOUT 10min"
-    bash /isaac-sim/omni.isaac.sim.post.install.sh
+    sudo bash /isaac-sim/omni.isaac.sim.post.install.sh
 fi                                                                                                                                                                                                                                                                                                      
 
 # Setup ZSH
 echo 'alias isaac=/isaac-sim/isaac-sim.sh' >> ~/.zshrc
-echo 'source /ros2_humble/install/setup.zsh' >> ~/.zshrc
 
 # Setup ROS2 Humble as the default bridge extension
 basekit_file="/isaac-sim/apps/omni.isaac.sim.base.kit"
