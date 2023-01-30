fac49f1bec4af13b805fa08e3638de0616d7a75c
Gage Miller <gagemiller155@gmail.com>
2023-01-30 01:25:12 -0500

    Add changes for using radians

--- rio/motion_magic_poc/robot.py
+++ rio/motion_magic_poc/robot.py
@@ -1,6 +1,7 @@
 import logging
 import wpilib
 import ctre
+import math
 
 # Ports
 motorPort = 7
@@ -10,7 +11,7 @@ encoderPort = 8
 pid_constants = {
     "kP": 0.2,
     "kI": 0.0,
-    "kD": 0.0,
+    "kD": 0.1,
     "kF": 0.2,
     "kIzone": 0,
     "kPeakOutput": 1.0
@@ -18,7 +19,12 @@ pid_constants = {
 slot_idx = 0
 pid_loop_idx = 0
 timeout_ms = 30
+velocityConstant = 1.0
+accelerationConstant = 0.5
 # Motor Constants
+drive_reduction = (14.0 / 50.0) * (25.0 / 19.0) * (15.0 / 45.0)
+steer_reduction = (15.0 / 32.0) * (10.0 / 60.0)
+ticks_per_rotation = 2048.0
 nominal_voltage = 12.0
 steer_current_limit = 20.0
 # Encoder Constants
@@ -26,13 +32,19 @@ encoder_ticks_per_rev = 2048
 encoder_offset = 0
 encoder_direction = False
 # Demo Behavior
-increment = 10
+increment = 1
+
+def get_radians_from_degrees(degrees):
+    return 
 
 ######### Robot Class #########
 class motor_poc(wpilib.TimedRobot):
 
     def robotInit(self) -> None:
         logging.info("Entering Robot Init")
+
+        self.positionCoefficient = 2.0 * math.pi / ticks_per_rotation * steer_reduction
+        self.velicityCoefficient = self.positionCoefficient * 10.0
 
         canCoderConfig = ctre.CANCoderConfiguration()
         canCoderConfig.absoluteSensorRange = ctre.AbsoluteSensorRange.Unsigned_0_to_360
@@ -43,12 +55,14 @@ class motor_poc(wpilib.TimedRobot):
         self.encoder.setPositionToAbsolute(timeout_ms)
         self.encoder.configAllSettings(canCoderConfig, timeout_ms)
         self.encoder.setStatusFramePeriod(ctre.CANCoderStatusFrame.SensorData, 10, timeout_ms)
+
+        
         
         self.joystick = wpilib.XboxController(controllerPort)
         self.talon = ctre.TalonFX(motorPort)
         self.talon.configFactoryDefault()
-        self.talon.configRemoteFeedbackFilter(self.encoder, 0, timeout_ms)
-        self.talon.configSelectedFeedbackSensor(ctre.TalonFXFeedbackDevice.RemoteSensor0, pid_loop_idx, timeout_ms)
+        # self.talon.configRemoteFeedbackFilter(self.encoder, 0, timeout_ms)
+        self.talon.configSelectedFeedbackSensor(ctre.TalonFXFeedbackDevice.IntegratedSensor, pid_loop_idx, timeout_ms)
         self.talon.configNeutralDeadband(0.001, timeout_ms)
         self.talon.setSensorPhase(True)
         self.talon.setInverted(False)
@@ -59,13 +73,14 @@ class motor_poc(wpilib.TimedRobot):
         self.talon.configPeakOutputForward(1, timeout_ms)
         self.talon.configPeakOutputReverse(-1, timeout_ms)
         self.talon.selectProfileSlot(slot_idx, pid_loop_idx)
-        self.talon.config_kF(slot_idx, pid_constants["kF"], timeout_ms)
         self.talon.config_kP(slot_idx, pid_constants["kP"], timeout_ms)
         self.talon.config_kI(slot_idx, pid_constants["kI"], timeout_ms)
         self.talon.config_kD(slot_idx, pid_constants["kD"], timeout_ms)
-        self.talon.configMotionCruiseVelocity(15, timeout_ms)
-        self.talon.configMotionAcceleration(5, timeout_ms)
-        # self.talon.setSelectedSensorPosition(0, pid_loop_idx, timeout_ms)
+        self.talon.config_kF(slot_idx, (1023.0 *  self.velicityCoefficient / nominal_voltage) * velocityConstant, timeout_ms)
+        self.talon.configMotionCruiseVelocity(2.0 / velocityConstant / self.velicityCoefficient, timeout_ms)
+        self.talon.configMotionAcceleration((8.0 - 2.0) / accelerationConstant / self.velicityCoefficient, timeout_ms)
+        
+        self.talon.setSelectedSensorPosition(math.radians(self.encoder.getAbsolutePosition()) / self.positionCoefficient, pid_loop_idx, timeout_ms)
         self.talon.configVoltageCompSaturation(nominal_voltage, timeout_ms)
         currentLimit = ctre.SupplyCurrentLimitConfiguration()
         currentLimit.enable = True
@@ -81,7 +96,7 @@ class motor_poc(wpilib.TimedRobot):
         logging.info("Entering Teleop")
     
     def teleopPeriodic(self) -> None:
-        currentPosition = self.talon.getSelectedSensorPosition()
+        motorPosition = self.talon.getSelectedSensorPosition() / self.positionCoefficient
         currentMotorOutput = self.talon.getMotorOutputPercent()
         currentVelocity = self.talon.getSelectedSensorVelocity()
         absolutePosition = self.encoder.getPosition()
@@ -93,7 +108,10 @@ class motor_poc(wpilib.TimedRobot):
 
         print(f"Target: {self.targetPosition} ABS POS: {absolutePosition}")
         # print(f"Target: {self.targetPosition} Current Position: {currentPosition} Current Motor Output: {currentMotorOutput} Current Velocity: {currentVelocity}")
-        self.talon.set(ctre.TalonFXControlMode.MotionMagic, self.targetPosition)
+        
+        # currentPosition = math.fmod(motorPosition, 2.0 * math.pi)
+        motorPostion = self.targetPosition 
+        self.talon.set(ctre.TalonFXControlMode.MotionMagic, self.targetPosition / self.positionCoefficient)
 
     def teleopExit(self) -> None:
         logging.info("Exiting Teleop")
