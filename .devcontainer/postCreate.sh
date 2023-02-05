ccc9b8cfe67d512f4c900dca2ad65369c93e4801
Gage Miller <gagemiller155@gmail.com>
2023-02-05 23:30:15 +0000

    add zsh settings

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -1,20 +1,3 @@
 #!/bin/bash
-
-# Configure omniverse cache 
-# sudo cp ./.devcontainer/omniverse.toml /root/.nvidia-omniverse/config/omniverse.toml
-
-# Run the postinstall script for isaac sim
-# Check to see if post install has already been run
-# if [ ! -d root/.cache/ov/Kit ]; then
-#     echo "ISAAC SIM POST INSTALL RUNNING... THIS WILL TAKE ABOUT 30min"
-#     sudo mkdir -p /root/Documents
-#     sudo chown -R root:root /root
-#     sudo /isaac-sim/omni.isaac.sim.post.install.sh
-# fi                                                                                                                                                                                                                                                                                                      
-
-# Setup ZSH
-# echo 'alias isaac=/isaac-sim/isaac-sim.sh' >> ~/.zshrc
-
-# Setup ROS2 Humble as the default bridge extension
-# basekit_file="/isaac-sim/apps/omni.isaac.sim.base.kit"
-# sudo sed -i 's/ros_bridge/ros2_bridge-humble/' $basekit_file
+git config devcontainers-theme.show-dirty 1
+sed -i 's/ZSH_THEME="devcontainers"/ZSH_THEME="eastwood"/' ~/.zshrc
