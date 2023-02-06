2e610cab0daaaa335d4959166d80689b9759bd13
Gage Miller <gagemiller155@gmail.com>
2023-02-06 00:38:54 +0000

    Add post create with ros sourced

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 git config devcontainers-theme.show-dirty 1
 sed -i 's/ZSH_THEME="devcontainers"/ZSH_THEME="eastwood"/' ~/.zshrc
+echo "source /opt/ros/humble/install/setup.zsh" >> ~/.zshrc
+echo "export ROS_DISTRO=humble" >> ~/.zshrc
+echo "export ROS_DOMAIN_ID=0" >> ~/.zshrc
