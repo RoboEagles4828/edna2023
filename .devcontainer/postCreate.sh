6c8cda97471b37d8eba4d1def84b7567116cb8a1
Gage Miller <gagemiller155@gmail.com>
2023-02-06 00:41:35 +0000

    Add updates to postcreate

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -4,3 +4,7 @@ sed -i 's/ZSH_THEME="devcontainers"/ZSH_THEME="eastwood"/' ~/.zshrc
 echo "source /opt/ros/humble/install/setup.zsh" >> ~/.zshrc
 echo "export ROS_DISTRO=humble" >> ~/.zshrc
 echo "export ROS_DOMAIN_ID=0" >> ~/.zshrc
+
+# Update
+sudo apt-get update
+rosdep update --rosdistro=humble
