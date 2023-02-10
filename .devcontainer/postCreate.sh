4f803d5931902407aa4c39444a69396aa357e4c0
Gage Miller <gagemiller155@gmail.com>
2023-02-10 19:50:34 +0000

    Add some quick workflow scripts

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -3,10 +3,11 @@ git config devcontainers-theme.show-dirty 1
 sed -i 's/ZSH_THEME="devcontainers"/ZSH_THEME="eastwood"/' ~/.zshrc
 
 echo "source /opt/ros/humble/setup.zsh" >> ~/.zshrc
+echo "source /workspaces/edna2023/install/setup.zsh" >> ~/.zshrc
 echo "export ROS_DISTRO=humble" >> ~/.zshrc
 echo "export ROS_DOMAIN_ID=0" >> ~/.zshrc
 
-# Update
+# Update apt list and rosdep
 sudo apt-get update
 rosdep update --rosdistro=humble
 rosdep install --from-paths src --ignore-src -r -y
--- .devcontainer/postStart.sh
+++ .devcontainer/postStart.sh
@@ -4,5 +4,17 @@
 sudo chmod a+rw /dev/input/js*
 source /opt/ros/humble/setup.bash
 
-echo "JOYSTICKS CONNECTED: "
+echo -e "\n----JOYSTICKS CONNECTED----"
 ros2 run joy joy_enumerate_devices
+
+echo -e "\n----ISAAC SIM COMMANDS----"
+echo "isaac-setup     : Setup Isaac Sim Shaders"
+echo "isaac           : Start Isaac Sim"
+echo "isaac rm        : Stop and delete isaac sim container"
+
+echo -e "\n----COMMANDS----"
+echo "launch          : Launch sim teleop code"
+echo "launch isaac    : Launch sim teleop code"
+echo "launch real     : Launch real teleop code"
+echo "restart-ros2    : restart ros2 daemon"
+echo "restart-ros2    : restart ros2 daemon"
