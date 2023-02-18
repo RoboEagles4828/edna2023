ebc6c22d7e086c780e62545c27c528179aea5950
nchan18 <nchan18@outlook.com>
2023-02-18 17:45:51 +0000

    single callback function not working

--- src/teleop_twist_joy/src/imu_listener.cpp
+++ src/teleop_twist_joy/src/imu_listener.cpp
@@ -1,5 +1,5 @@
-#include "ros/ros.h"
-#include "sensor_msgs/Imu.h"
+#include "rclcpp/rclcpp.hpp"
+#include "nav_msgs/msg/odometry.hpp"
 
 /**
  * This tutorial demonstrates simple receipt of IMU sensor data over the ROS system.
@@ -9,7 +9,7 @@
  * Callback function executes when new topic data comes.
  * Task of the callback function is to print data to screen.
  */
-void chatterCallback(const sensor_msgs::Imu::ConstPtr& msg)
+void chatterCallback(const nav_msgs::msg::Odometry msg)
 {
   ROS_INFO("Imu Seq: [%d]", msg->header.seq);
   ROS_INFO("Imu Orientation x: [%f], y: [%f], z: [%f], w: [%f]", msg->orientation.x,msg->orientation.y,msg->orientation.z,msg->orientation.w);
@@ -17,14 +17,21 @@ void chatterCallback(const sensor_msgs::Imu::ConstPtr& msg)
 
 int main(int argc, char **argv)
 {
+
+  const nav_msgs::msg::Odometry empty_odometry;
+  auto qos = rclcpp::QoS(1);
+  qos.best_effort();
+  rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odom_subscriber;
+  odom_subscriber = node_->create_subscription<sensor_msgs::msg::JointState>(joint_state_topic_, qos,
+    [this](const std::shared_ptr<nav_msgs::msg::Odometry> msg) -> void
+    {
+      if (!subscriber_is_active_) {
+        RCLCPP_WARN( rclcpp::get_logger("isaac_hardware_interface"), "Can't accept new commands. subscriber is inactive");
+        return;
+      }
+      received_joint_msg_ptr_.set(std::move(msg));
+    });
   
-  ros::init(argc, argv, "imu_listener");
-
-  ros::NodeHandle n;
-
-  ros::Subscriber sub = n.subscribe("odom", 1000, chatterCallback);
-
-  ros::spin();
 
   return 0;
 }
