e53e7508712a77226de0c08ae285d4cd6ae9544c
nchan18 <nchan18@outlook.com>
2023-03-14 22:08:12 +0000

    frc_auton update

--- src/frc_auton/frc_auton/reader.py
+++ src/frc_auton/frc_auton/reader.py
@@ -2,19 +2,21 @@ from rosbags.rosbag2 import Reader
 from rosbags.serde import deserialize_cdr
 import rclpy
 from rclpy.node import Node
-
-from std_msgs.msg import String
+from geometry_msgs.msg import Twist
+from std_msgs.msg import Bool
 
 
 class StageSubscriber(Node):
 
     def __init__(self):
         super().__init__('stage_subscriber')
-        self.subscription = self.create_subscription(bool,'frc_stage',self.listener_callback,10)
+        self.subscription = self.create_subscription(Bool,'frc_stage',self.listener_callback,10)
+        self.publisher = self.create_publisher(Twist,'cmd_vel',10)
         self.subscription  # prevent unused variable warning
 
     def listener_callback(self, msg):
-        if msg and msg.data==1:
+        print(msg)
+        if msg==True:
             with Reader('Auto_ros_bag/rosbag2_2023_03_11-01_22_57_0.db3') as reader:
                 # topic and msgtype information is available on .connections list
                 for connection in reader.connections:
@@ -24,7 +26,8 @@ class StageSubscriber(Node):
                 for connection, timestamp, rawdata in reader.messages():
                     if connection.topic == 'cmd_vel':
                         msg = deserialize_cdr(rawdata, connection.msgtype)
-                        print(msg.header.frame_id)
+                        self.publisher.publish(msg)
+                        print("publishing")
 
 
 def main(args=None):
