66bfb76bcccba59f7e1875f4fd533c4460b1cae9
SarangaR <sarnga.raj@gmail.com>
2023-03-18 19:11:19 +0000

    joint_trajectory_teleop fixes

--- src/frc_auton/setup.py
+++ src/frc_auton/setup.py
@@ -11,7 +11,10 @@ setup(
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
     ],
-    install_requires=['setuptools','rosbag'],
+    install_requires=['setuptools','rosbags'],
+    extras_require={
+        'rosbags': ['rosbags']
+    },
     zip_safe=True,
     maintainer='admin',
     maintainer_email='nchan18@outlook.com',
--- src/joint_trajectory_teleop/joint_trajectory_teleop/joint_trajectory_teleop.py
+++ src/joint_trajectory_teleop/joint_trajectory_teleop/joint_trajectory_teleop.py
@@ -1,6 +1,7 @@
 import rclpy
 from rclpy.node import Node
 from rclpy.qos import QoSProfile, QoSHistoryPolicy, QoSDurabilityPolicy, QoSReliabilityPolicy
+from rclpy import logging
 import math
 
 from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
@@ -9,6 +10,7 @@ import os
 from rclpy.qos import QoSDurabilityPolicy, QoSHistoryPolicy, QoSReliabilityPolicy, QoSProfile
 
 import time
+import yaml
 
 class toggleButton():
     def __init__(self, button):
@@ -46,90 +48,75 @@ class PublishTrajectoryMsg(Node):
             'bottom_gripper_left_arm_joint',
         ]
 
-        self.button_dict = {
-            'A': 0,
-            'B': 1,
-            'X': 2,
-            'Y': 3,
-            'LB': 4,
-            'RB': 5,
-            'MENU': 7,
-            'SQUARES': 6,
-            'RIN': 10
-        }
-        self.axis_dict = {
-            'DPAD_Y': 7,
-            'DPAD_X': 6,
-            'LT': 2,
-            'RT': 5,
-        }
-
         self.pos = 0.0
         self.rot = 0.0
 
-        # qos = QoSProfile(reliability=QoSReliabilityPolicy.BEST_EFFORT, durability=QoSDurabilityPolicy.VOLATILE, history=QoSHistoryPolicy.KEEP_LAST, depth=10)
         self.publisher_ = self.create_publisher(JointTrajectory, 'joint_trajectory_controller/joint_trajectory', 10)
         self.subscriber = self.create_subscription(Joy, 'joy', self.controller_callback, 10)
         self.timer_period = 0.5  # seconds
-        self.aButton = toggleButton(self.button_dict['A'])
-        self.bButton = toggleButton(self.button_dict['B'])
-        self.lbButton = toggleButton(self.button_dict['LB'])
+
+        # Load yaml
+        self.yaml_path = '/workspaces/edna2023/src/edna_bringup/config/teleop-control.yaml'
+        with open(self.yaml_path, 'r') as f:
+            self.yaml = yaml.safe_load(f)
+
+        self.functions = [self.elevator_loading_station]
+
+        self.cmds: JointTrajectory = JointTrajectory()
+        self.position_cmds: JointTrajectoryPoint = JointTrajectoryPoint()
+        self.position_cmds.positions = [0.0] * len(self.joints)
+
+        self.cmds.joint_names = self.joints
+
+        self.joint_map = self.yaml['joint_mapping']
 
     def controller_callback(self, joystick: Joy):
-        cmds = JointTrajectory()
-        position_cmds = JointTrajectoryPoint()
-        # self.get_logger().info('\nBUTTONS: ' + str(joystick.buttons) + '\nAXES: ' + str(joystick.axes))
-        
-        aToggleValue = self.aButton.toggle(joystick.buttons)
-        bToggleValue = self.bButton.toggle(joystick.buttons)
-        lbToggleValue = self.lbButton.toggle(joystick.buttons)
+        for function in self.functions:
+            button = self.yaml['controller_mapping'][self.yaml['function_mapping'][function.__name__]['button']]
+            toggle = self.yaml['function_mapping'][function.__name__]['toggle']
 
-        if joystick.buttons[self.button_dict['LB']] == 1.0:
-            self.pos = 1.1
-        elif joystick.buttons[self.button_dict['RIN']] == 1.0:
-            self.pos = 0.2
-        elif lbToggleValue:
-            self.pos = 0.2
-        elif joystick.buttons[self.button_dict['RIN']] == 1.0:
-            self.pos = 0.0
+            function(joystick, button, toggle)
+            logging.get_logger('joint-trajectory-teleop-publisher').info(str(self.cmds))
+            self.publisher_.publish(self.cmds)
 
-        if joystick.buttons[self.button_dict['Y']] == 1.0:
-            self.rot = 0.1
+    def elevator_loading_station(self, joystick: Joy, button: int, toggle: bool):
+
+        if toggle:
+            button_val = float(toggleButton(button).toggle(joystick.buttons))
         else:
-            self.rot = 0.0
+            button_val = float(joystick.buttons[button])
+        
+        #TODO: Tweak the values
+        if button_val == 1.0:
+            self.position_cmds.positions[int(self.joint_map['elevator_center_joint'])] = 0.5
+            self.position_cmds.positions[int(self.joint_map['elevator_outer_2_joint'])] = 0.5
+            self.position_cmds.positions[int(self.joint_map['top_slider_joint'])] = 1.0
+        elif button_val == 0.0:
+            self.position_cmds.positions[int(self.joint_map['elevator_center_joint'])] = 0.0
+            self.position_cmds.positions[int(self.joint_map['elevator_outer_2_joint'])] = 0.0
+            self.position_cmds.positions[int(self.joint_map['top_slider_joint'])] = 0.0
+        
+        self.cmds.points = [self.position_cmds]
+
+    # def skis_up(self, joystick: Joy, button: int, toggle: bool):
+    #     if toggle:
+    #         button_val = float(toggleButton(button).toggle(joystick.buttons))
+    #     else:
+    #         button_val = float(joystick.buttons[button])
+        
+    #     #TODO: Tweak the values
+    #     if button_val == 1.0:
+    #         self.position_cmds.positions[self.joint_map['bottom_intake_joint']] = 0.5
+    #     elif button_val == 0.0:
+    #         self.position_cmds.positions[self.joint_map['elevator_center_joint']] = 0.0
+    #         self.position_cmds.positions[self.joint_map['elevator_outer_2_joint']] = 0.0
+    #         self.position_cmds.positions[self.joint_map['top_slider_joint']] = 0.0
+
+    
         
 
-        # self.joints = [
-        #     'arm_roller_bar_joint',
-        #     'elevator_center_joint',
-        #     'elevator_outer_1_joint',
-        #     'elevator_outer_2_joint',
-        #     'top_gripper_right_arm_joint',
-        #     'top_gripper_left_arm_joint',
-        #     'top_slider_joint',
-        #     'bottom_intake_joint',
-        #     'bottom_gripper_right_arm_joint',
-        #     'bottom_gripper_left_arm_joint',
-        # ]
 
-        position_cmds.positions = [
-            float(joystick.buttons[self.button_dict['Y']]),
-            self.pos,
-            self.rot,
-            self.pos,
-            float(aToggleValue),
-            float(aToggleValue),
-            float(bToggleValue),
-            float(joystick.buttons[self.button_dict['A']]),
-            float(joystick.axes[self.axis_dict['RT']]),
-            float(joystick.axes[self.axis_dict['RT']]),
-        ]
-        
-        cmds.joint_names = self.joints
-        cmds.points = [position_cmds]
-        
-        self.publisher_.publish(cmds)
-        # self.get_logger().info('Publishing...')
+
 
 def main(args=None):
     rclpy.init(args=args)
