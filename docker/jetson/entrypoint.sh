587f7047807f8e87411753fbe47fd8d24a2ac345
Gage Miller <gagemiller155@gmail.com>
2023-03-20 21:26:16 -0400

    Fix entrypoint 3/20 orin

--- docker/jetson/entrypoint.sh
+++ docker/jetson/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 source /opt/ros/${ROS_DISTRO}/setup.bash
 
-colcon build --symlink-install
+colcon build --symlink-install --paths src
 
 source install/setup.bash
 ros2 launch edna_bringup real.launch.py
