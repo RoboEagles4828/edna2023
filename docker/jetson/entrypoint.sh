3b2b928fccb79ed80a75f6d4bcac0814e49f4c65
ugackMiner <ugackMiner@gmail.com>
2023-03-23 23:09:24 +0000

    Random jetson changes

--- docker/jetson/entrypoint.sh
+++ docker/jetson/entrypoint.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 source /opt/ros/${ROS_DISTRO}/setup.bash
 
-colcon build --symlink-install --paths src/*
-
+colcon build --symlink-install --event-handlers --paths src/* --packages-skip edna_tests edna_debugger
 source install/setup.bash
 ros2 launch edna_bringup real.launch.py
