eaffedbdfcb319f226338254ebf172195d1e11fb
SarangaR <sarnga.raj@gmail.com>
2023-03-25 12:13:02 +0000

    thread stuff

--- src/frc_auton/frc_auton/writer.py
+++ src/frc_auton/frc_auton/writer.py
@@ -40,6 +40,8 @@ class StartWriting(Node):
         self.recorder = rosbag2_py.Recorder()
         self.swerve_record_options.topics = ['swerve_controller/cmd_vel_unstamped', 'joint_trajectory_controller/joint_trajectory']
 
+        self.is_recording = False
+
     def service_callback(self, request, response):
         self.service_enabled = request.record
         self.kill = request.kill
@@ -72,64 +74,21 @@ class StartWriting(Node):
         # self.get_logger().info(f"THREAD STATUS: {self.bag_writer.get_thread_status()}")
 
     def bag_record(self):
-        self.recorder.record(self.storage_options, self.swerve_record_options)
+        if self.is_recording == False:
+            self.is_recording = True
+            self.recorder.record(self.storage_options, self.swerve_record_options)
 
     def start_record_thread(self):
         self.record_thread = Thread(target=self.bag_record, daemon=True)
+        self.is_recording = True
         self.record_thread.start()
 
     def stop_record_thread(self):
+        self.is_recording = False
         self.record_thread.join()
     
     def get_thread_status(self):
         return self.record_thread.is_alive()
-      
-
-class BagWriter(): 
-    def __init__(self):
-        self.curr_file_path = os.path.abspath(__file__)
-        self.project_root_path = os.path.abspath(os.path.join(self.curr_file_path, "../../../.."))
-        self.package_root = os.path.join(self.project_root_path, 'src/frc_auton')
-
-        # self.subscription_arm = self.create_subscription(JointTrajectory,'joint_trajectory_controller/joint_trajectory',self.arm_callback,10)
-        # self.subscription_swerve = self.create_subscription(Twist,'swerve_controller/cmd_vel_unstamped',self.swerve_callback,10)
-
-        file_counter= int(len(os.listdir(f'{self.package_root}/frc_auton/Auto_ros_bag')))
-        self.path = f'{self.package_root}/frc_auton/Auto_ros_bag/bag_'+str(file_counter)
-       
-        self.writer = rosbag2_py.SequentialWriter()
-        self.storage_options = rosbag2_py._storage.StorageOptions(uri=self.path,storage_id='sqlite3')
-        self.converter_options = rosbag2_py._storage.ConverterOptions('', '')
-        self.swerve_record_options = rosbag2_py._transport.RecordOptions()
-        self.arm_record_options = rosbag2_py._transport.RecordOptions()
-        # self.writer.open(storage_options, converter_options)
-        # topic_info_arm = rosbag2_py._storage.TopicMetadata(name=self.subscription_arm.topic_name,type='trajectory_msgs/msg/JointTrajectory',serialization_format='cdr')
-        # self.writer.create_topic(topic_info_arm)
-
-        # topic_info_swerve = rosbag2_py._storage.TopicMetadata(name=self.subscription_swerve.topic_name,type='geometry_msgs/msg/Twist',serialization_format='cdr')
-        # self.writer.create_topic(topic_info_swerve)
-
-        self.recorder = rosbag2_py.Recorder()
-        self.swerve_record_options.topics = ['real/swerve_controller/cmd_vel_unstamped', 'real/joint_trajectory_controller/joint_trajectory']
-        # self.arm_record_options.topics = [self.subscription_arm.topic_name]
-
-        
-    # def swerve_callback(self, msg):
-    #     self.recorder.record(self.storage_options, self.swerve_record_options)
-
-    def bag_record(self):
-        self.recorder.record(self.storage_options, self.swerve_record_options)
-
-    def start_record_thread(self):
-        self.record_thread = Thread(target=self.bag_record, daemon=True)
-        self.record_thread.start()
-
-    def stop_record_thread(self):
-        self.record_thread.join()
-    
-    def get_thread_status(self):
-        return self.record_thread.is_alive()
-
 
 
     
